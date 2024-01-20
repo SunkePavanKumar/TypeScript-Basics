c1146f0c1b95b388133e92c89f057b2e4450d980
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-01-20 14:51:37 +0530

    Inrterfaces TS

--- interfaces.ts
+++ interfaces.ts
@@ -1,4 +1,34 @@
-class Animal {
-    name: string;
-    age : number;
+interface patientDetails {
+  name: string;
+  age: number;
+  height: number;
+  weight: number;
+  sex: string;
 }
+
+// If the user need to add the additional features( Note: in the real scenario it might be in the other file)
+
+interface patientDetails {
+  bp: string;
+}
+
+// Inherit the properties of the patient
+interface nomineeDetails extends patientDetails {
+  nomineeName: string;
+  mobile: number | string;
+}
+
+let patient: patientDetails = {
+  name: "pavan kumar sunke",
+  age: 22,
+  height: 5.67,
+  weight: 85,
+  sex: "male",
+  bp: "176/180",
+};
+
+let nominee: nomineeDetails = {
+  nomineeName: "Test Pavan",
+  mobile: "93475*****",
+  ...patient,
+};
